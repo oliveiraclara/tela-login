a2367c066ca38cad1436f2b1d3d3f2e78b3c3291
oliveiraclara <110606333+oliveiraclara@users.noreply.github.com>
2023-03-14 10:33:10 -0300

    i

--- app/src/main/java/br/senai/sp/jandira/tripapp/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/tripapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package br.senai.sp.jandira.tripapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -13,6 +16,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TripAppTheme {
-                TripAppScreen()
+                LoginScreen()
             }
         }
     }
@@ -35,7 +39,9 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showSystemUi = true)
 @Composable
-fun TripAppScreen(){
+fun LoginScreen() {
+
+    val context = LocalContext.current
 
     var emailUser by rememberSaveable {
         mutableStateOf("")
@@ -44,23 +50,27 @@ fun TripAppScreen(){
         mutableStateOf("")
     }
 
-    
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            //Header
-            Card(
-                modifier = Modifier
-                    .height(50.dp)
-                    .width(120.dp),
-                backgroundColor = Color(red = 207, green = 6, blue = 240),
-            ) {
-            }
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(24.dp)
-            ) {
-                //Form
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement  = Arrangement.SpaceEvenly
+        ){
+            {
+                Column() {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Card(
+                            modifier = Modifier
+                                .width(140.dp)
+                                .height(40.dp),
+                            shape = RoundedCornerShape(bottomStart = 16.dp),
+                            backgroundColor = Color(red = 207, green = 6, blue = 240)
+                        ) {
+                        }
+                    }
+                }
                 Column(modifier = Modifier.fillMaxWidth()) {
                     Text(
                         text = stringResource(id = R.string.title),
@@ -73,27 +83,39 @@ fun TripAppScreen(){
                         text = stringResource(id = R.string.please_sign_up),
                         color = Color(red = 160, green = 156, blue = 156)
                     )
-                }
-                Spacer(modifier = Modifier.height(87.dp))
-                Column()
-                {
                     OutlinedTextField(value = emailUser, onValueChange = { emailUser = it },
                         modifier = Modifier.fillMaxWidth(),
                         shape = RoundedCornerShape(16.dp),
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        label = { Text(text = stringResource(id = R.string.email)) }
+                        label = { Text(text = stringResource(id = R.string.email)) },
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.email_24),
+                                contentDescription = stringResource(id = R.string.email_description),
+                                tint = Color(207, 1, 240)
+                            )
+                        }
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
+                    //Spacer(modifier = Modifier.height(16.dp))
                     OutlinedTextField(value = passwordUser, onValueChange = { passwordUser = it },
                         modifier = Modifier.fillMaxWidth(),
-                        //Image(painter = painterResource(id = R.drawable.email), contentDescription = "" ),
                         shape = RoundedCornerShape(16.dp),
                         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        label = { Text(text = stringResource(id = R.string.password)) }
+                        label = {
+                            Text(text = stringResource(id = R.string.password))
+                        },
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.lock_24),
+                                contentDescription = stringResource(id = R.string.email_description),
+                                tint = Color(207, 1, 240)
+                            )
+                        }
                     )
-                    Spacer(modifier = Modifier.height(30.dp))
                     Column(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
                         horizontalAlignment = Alignment.End
                     ) {
                         Button(
@@ -101,14 +123,28 @@ fun TripAppScreen(){
                             modifier = Modifier
                                 .width(134.dp)
                                 .height(48.dp),
+                            colors = ButtonDefaults.buttonColors(Color(207, 6, 246)),
                             shape = RoundedCornerShape(15.dp)
                         ) {
-                            Text(
-                                text = stringResource(id = R.string.sign_in),
-                                fontWeight = FontWeight.Bold
-                            )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = stringResource(id = R.string.sign_in),
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.White
+                                )
+                                Icon(
+                                    painter = painterResource(
+                                        id = R.drawable.arrow_forward_24
+                                    ),
+                                    contentDescription = "",
+                                    tint = Color.White
+                                )
+                            }
                         }
-                        Spacer(modifier = Modifier.height(31.dp))
                         Row() {
                             Text(
                                 text = stringResource(id = R.string.not_have_account),
@@ -117,16 +153,31 @@ fun TripAppScreen(){
                             Spacer(modifier = Modifier.width(4.dp))
                             Text(
                                 text = stringResource(id = R.string.sign_up),
+                                //modifier = Modifier.clickable {
+                                // val intent = Intent(context, SignUpActivity)
+                                //},
                                 color = Color(red = 207, green = 6, blue = 240),
                                 fontWeight = FontWeight.Bold
                             )
                         }
                     }
                 }
-                //Footer
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Card(
+                            modifier = Modifier
+                                .width(140.dp)
+                                .height(40.dp),
+                            shape = RoundedCornerShape(topEnd = 16.dp),
+                            backgroundColor = Color(red = 207, green = 6, blue = 240)
+                        ) {
+                        }
+                    }
+                }
             }
         }
     }
-    
-}
+    }
